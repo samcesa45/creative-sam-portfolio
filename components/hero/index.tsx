a2e71dc776fe8a98f76f7936a3edb77d729c1faa
samcesa45 <samcesa45@gmail.com>
2023-08-28 11:47:48 +0100

    minor fixes

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,14 +1,9 @@
-"use client";
-import { styles } from "@/styles";
+"use client";;
 import Link from "next/link";
-import ComputersCanvas from "../canvas/Computers";
 import { motion } from "framer-motion";
-import { useContext } from "react";
-import { ThemeContext } from "@/context";
 import "aos/dist/aos.css";
 import { useTheme } from "next-themes";
 export default function Hero() {
-  // const { theme } = useContext(ThemeContext);
   const { theme } = useTheme();
   return (
     <section className="relative w-full h-screen mx-auto ">
@@ -46,7 +41,7 @@ export default function Hero() {
       {/* <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <Link href="#about">
+        <Link href="#about" passHref>
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{ y: [0, 24, 0] }}
